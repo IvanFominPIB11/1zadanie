96c5050f67ab3233bd9c03eade73d76a65be89ed
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-15 17:25:11 +0700

    Добавьте файлы проекта.

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1zadanie
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name;
+            Console.Title = "Давайте познакомимся...";
+            Console.Write("Как Вас зовут?");
+            name = Console.ReadLine();
+            string surname;
+            Console.Write("Ваша Фамилия? Перед тем как написать, нажмите пробел и вводите фамилию");
+            surname = Console.ReadLine();
+            string txt = "Очень приятно, " + name + surname + "!";
+            Console.WriteLine(txt);
+        }
+    }
+}
